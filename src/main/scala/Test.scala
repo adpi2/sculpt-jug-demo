d56d734db146a31eaafadbefe212357081384d11
Adrien Piquerez <apiquerez@groupcls.com>
2020-01-15 13:46:23 +0100

    init demo

--- src/main/scala/Test.scala
+++
@@ -1,7 +0,0 @@
-import sculpt.Potential
-import sculpt.scalatest.ScalaTestSculpting
-import sculpt.scalatest.Spec
-
-class Test extends ScalaTestSculpting {
-  val potential: Potential.Any[Int, Spec] = Potential.Any[Int, Spec]
-}
---
+++ src/test/scala/conway/GameOfLifeSuite.scala
@@ -0,0 +1,5 @@
+package conway
+
+class GameOfLifeSuite {
+
+}
